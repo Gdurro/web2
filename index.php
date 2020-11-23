80d5562f59f87ea59d21b4c615e83e0cd859095e
Gdurro <grzegorzchrabel@gmail.com>
2020-11-23 21:31:57 +0100

    test

--- index.php
+++ index.php
@@ -6,6 +6,21 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Język Angielski</h1>
+    <header>
+		<h1 class="logo">Angielski.pl</h1>
+		<nav id="topnav">
+		
+			<ul class="menu">
+				<li><a href="index.php">Strona główna</a></li>
+				<li><a href="#">Regulamin</a></li>
+				<li><a href="#">Premium</a></li>
+				<li><a href="#">Zaloguj się</a></li>
+			</ul>
+			
+		</nav>
+	
+
+	
+	</header>
 </body>
 </html>
