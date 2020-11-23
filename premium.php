edae83688b76ea62611a25fa260b620a4808dbbe
Kubson007 <jakubtomaszewski2007@gmail.com>
2020-11-23 22:07:43 +0100

    Premium

---
+++ premium.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset-"utf-8"/>
+	<title>Zawartość pakietu Premium</title>
+	<meta name="description" content="Tutaj znajdziesz informacje o zawartości pakietu premium"/>
+	<meta name="keywords" content="Premium, pakiet"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+</head>
+
+<body>
+
+<header>
+		<h1 class="logo">Premium</h1>
+		<nav id="topnav">
+		
+			<ul class="menu">
+				<li><a href="index.php">Strona główna</a></li>
+				<li><a href="#">Zaloguj się</a></li>
+			</ul>
+			
+		</nav>
+	
+
+	
+	</header>
+
+</body>
+</html>
