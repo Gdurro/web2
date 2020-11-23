72f6d7c425bbfecbd162639ea86970c5532c857b
ImperatorBanan <brian.lodz@gmail.com>
2020-11-23 21:59:14 +0100

    lol

---
+++ info.php
@@ -0,0 +1,15 @@
+<html> 
+<head>
+      <title>Na czym to polega?</title>
+</head>
+
+<body>
+     <h1>Informacje</h1>
+	 
+	 <h3>Nauka angielskiego przydaje sie w rzyciu  dosyć często, dlatego powstała ta strona. pomorzemy ci się nauczyć angielskiego, w szybki sposób. Fiszki, czyli dopasowywanie odpowiednich wyrazów do obrazków jest dobrą i przyjemną formą nauki jakiego kolwiek języka obcego</h3>
+	 
+	 
+	 
+</body>
+
+</html>
