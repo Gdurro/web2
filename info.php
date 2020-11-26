fee5379bab27cc1b402c7d0c89bb71f27f4f8fd7
Kubson007 <jakubtomaszewski2007@gmail.com>
2020-11-26 20:49:59 +0100

    Poprawione :)

--- info.php
+++ info.php
@@ -6,7 +6,7 @@
 <body>
      <h1>Informacje</h1>
 	 
-	 <h3>Nauka angielskiego przydaje sie w rzyciu  dosyć często, dlatego powstała ta strona. pomorzemy ci się nauczyć angielskiego, w szybki sposób. Fiszki, czyli dopasowywanie odpowiednich wyrazów do obrazków jest dobrą i przyjemną formą nauki jakiego kolwiek języka obcego</h3>
+	 <h3>Nauka angielskiego przydaje sie w życiu  dosyć często, dlatego powstała ta strona. pomożemy ci się nauczyć angielskiego, w szybki sposób. Fiszki, czyli dopasowywanie odpowiednich wyrazów do obrazków jest dobrą i przyjemną formą nauki jakiego kolwiek języka obcego</h3>
 	 
 	 
 	 
